6b68a466c444a3cefcbca08272931a836c987e59
mayala <mayala@digitalgeko.com>
2025-10-25 17:07:46 -0600

    Estructura inicial del proyecto

---
+++ src/test/java/com/microservicios_test/microservicios_test/MicroserviciosTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicios_test.microservicios_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
